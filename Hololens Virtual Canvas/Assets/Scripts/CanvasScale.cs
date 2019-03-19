2c873de227de35a0260e51b17fa554f0fd892e27
Nolan Cooper <nolancooper97@gmail.com>
2019-03-18 20:55:59 -0500

    Canvas Scaling working properly now

--- Hololens Virtual Canvas/Assets/Scripts/CanvasScale.cs
+++ Hololens Virtual Canvas/Assets/Scripts/CanvasScale.cs
@@ -4,24 +4,19 @@ using System;
 using UnityEngine;
 
 public class CanvasScale : MonoBehaviour {
+    public GameObject object1;
+    public GameObject object2;
 
-    public Vector3 Vec1;
-    public Vector3 Vec2;
-    public Transform target;
-
-	// Use this for initialization
-	void Start () {
-		Vector3 between = Vec2 - Vec1;
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos1 = object1.transform.position;
+        Vector3 pos2 = object2.transform.position;
+        Vector3 between = pos1 - pos2;
+  
         float xDist = Math.Abs(between.x);
         float yDist = Math.Abs(between.y);
         float zDist = Math.Abs(between.z);
-        transform.localScale += new Vector3(xDist, yDist, zDist);
-        transform.position = Vec1 + (between / 2.0F);
-        transform.LookAt(target);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.LookAt(target);
+        transform.localScale = new Vector3(xDist, yDist, zDist);
+        transform.position = pos1 - (between / 2.0F);
     }
 }
