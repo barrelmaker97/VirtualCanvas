3abe51796ef68d666fafa2f6596690fdb2aa96a2
Nolan Cooper <nolancooper97@gmail.com>
2019-04-22 19:59:26 -0500

    Code cleanup

--- Hololens Virtual Canvas/Assets/VirtualCanvas/Scripts/BackClick.cs
+++ Hololens Virtual Canvas/Assets/VirtualCanvas/Scripts/BackClick.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 using UnityEngine.SceneManagement;
-using Vuforia;
 
 public class BackClick : MonoBehaviour, IInputClickHandler
 {
