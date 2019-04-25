5f98fb5e46fe75380f77bfd66ec2721a136d171f
Nolan Cooper <nolancooper97@gmail.com>
2019-04-24 20:45:11 -0500

    Added gyroscope function

---
+++ Hololens Virtual Canvas/Assets/VirtualCanvas/Scripts/CameraPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour {
+
+    Gyroscope m_Gyro;
+
+    void Start()
+    {
+        //Set up and enable the gyroscope (check your device has one)
+        m_Gyro = Input.gyro;
+        m_Gyro.enabled = true;
+    }
+
+    void Update ()
+    {
+        GyroModifyCamera();
+    }
+
+    void GyroModifyCamera()
+    {
+        transform.rotation = GyroToUnity(m_Gyro.attitude);
+    }
+
+    private static Quaternion GyroToUnity(Quaternion q)
+    {
+        return new Quaternion(q.x, q.y, -q.z, -q.w);
+    }
+}
