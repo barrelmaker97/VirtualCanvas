15f1a24f5d739a312db947bfb1601800e20f3206
Nolan Cooper <nolancooper97@gmail.com>
2019-05-01 23:01:42 -0500

    Updated text on canvas

---
+++ Hololens Virtual Canvas/Assets/VirtualCanvas/Scripts/GetCubeSize.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class GetCubeSize : MonoBehaviour {
+
+    private GameObject canvas;
+    private Transform trans;
+    private TextMesh display;
+
+	// Use this for initialization
+	void Start ()
+    {
+        canvas = this.transform.parent.gameObject;
+        trans = canvas.transform;
+        display = this.GetComponent<TextMesh>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        double width = Math.Round(trans.localScale.x * 100, 1);
+        double height = Math.Round(trans.localScale.y * 100, 1);
+        double depth = Math.Round(trans.localScale.z * 100, 1);
+        display.text = "Width: " + width.ToString() + "cm" + "\nHeight: " + height.ToString() + "cm" + "\nDepth: " + depth.ToString() + "cm";
+	}
+}
